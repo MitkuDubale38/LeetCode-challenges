e0495fb466c195dd98cce100f34f5756c0c80cc8
Mitku Dubale <60028209+MitkuDubale38@users.noreply.github.com>
2023-10-06 10:24:22 +0300

    Time: 274 ms (6.97%), Space: 148.5 MB (13.43%) - LeetHub

--- 0066-plus-one/0066-plus-one.dart
+++ 0066-plus-one/0066-plus-one.dart
@@ -1,9 +1,6 @@
 class Solution {
    List<int> plusOne(List<int> digits) {
-    String num = "";
-    for(int i = 0; i< digits.length; i++){
-      num = num + digits[i].toString();
-    }
+   String num = digits.map((digit) => digit.toString()).join();
      BigInt addedOneNum = BigInt.parse(num) + BigInt.parse("1");
     List<int> results = addedOneNum.toString().split('').map((String element) => int.parse(element)).toList();
    return results;
